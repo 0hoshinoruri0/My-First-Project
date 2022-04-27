95275352bdffaa7d343341dc1526811379f96273
0hoshinoruri0 <104482900+0hoshinoruri0@users.noreply.github.com>
2022-04-27 13:19:53 +0800

    Add files via upload

---
+++ BeeperRowAdv.py
@@ -0,0 +1,53 @@
+"""
+File: BeeperRowAdv.py
+Name:
+------------------------------
+This program makes Karel fill up
+Street 1 with some beepers already
+existed
+(This should be world insensitive)
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will fill the first Street in any world
+    """
+    while front_is_clear():
+        if on_beeper():
+            move()
+        else:
+            put_beeper()
+            move()
+        if on_beeper():
+           pass
+        else:
+           put_beeper()
+    pass
+    if front_is_clear():
+        pass
+    else:
+        turn_left()
+    while front_is_clear():
+         put_beeper()
+         move()
+    turn_left()
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
